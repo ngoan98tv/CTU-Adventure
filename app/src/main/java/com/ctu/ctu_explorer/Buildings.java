4a39326b2948679dbd4f358cd6c2ef4d47737056
Dracoilmkt <49397853+Dracoilmkt@users.noreply.github.com>
2019-08-14 12:36:20 +0700

    Updated locations lat-long

--- app/src/main/java/com/ctu/ctu_explorer/Buildings.java
+++ app/src/main/java/com/ctu/ctu_explorer/Buildings.java
@@ -12,6 +12,15 @@ public final class Buildings {
             new GeoPoint(10.031334, 105.765887),
             new GeoPoint(10.031873, 105.766472),
             new GeoPoint(10.027092, 105.764778),
-            new GeoPoint(10.032174, 105.770624)
+            new GeoPoint(10.032174, 105.770624),
+            new GeoPoint(10.029522, 105.770268),
+            new GeoPoint(10.030040, 105.771756),
+            new GeoPoint(10.032316, 105.771295),
+            new GeoPoint(10.030922, 105.772571),
+            new GeoPoint(10.028064, 105.768478),
+            new GeoPoint(10.032026, 105.768885),
+            new GeoPoint(10.028011, 105.767786),
+            new GeoPoint(10.033753, 105.769973),
+            new GeoPoint(10.029728, 105.764493)
     };
 }
